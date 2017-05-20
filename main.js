ce9ac548a15cfe12a6cd8dcdfb09f39ad88bc75c
XC3S <m.geithe@gmx.de>
2017-05-20 14:23:34 +0200

    invisible crawler

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require('electron');
 
 app.on('ready', function(){
-	var listCrawler = listCrawler || new BrowserWindow({width: 1200, height: 800, show: true});	
+	var listCrawler = listCrawler || new BrowserWindow({width: 1200, height: 800, show: false});	
 	//listCrawler.webContents.openDevTools();
 
 	var crawler = require("./crawler/crawler.js")(app, listCrawler);
